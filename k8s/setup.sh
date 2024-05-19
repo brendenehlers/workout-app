06abeb607d0c010470692fe768b72fb1ead8b1b6
Brenden Ehlers <brenden@theehlers.org>
2024-05-19 09:04:39 -0600

    update k8s

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -6,16 +6,16 @@ ENV=dev
 # vars
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# configure ECK
-ECK_VERSION=2.12.1
-
-kubectl create -f https://download.elastic.co/downloads/eck/$ECK_VERSION/crds.yaml
-kubectl apply -f https://download.elastic.co/downloads/eck/$ECK_VERSION/operator.yaml
-# create logging infrastructure
-kubectl apply -Rf $SCRIPT_DIR/logging/
-
 # env-specfic config
 /$SCRIPT_DIR/$ENV/setup.sh
 
-# apply env
+# provision logging
+ECK_VERSION=2.12.1
+kubectl create -f https://download.elastic.co/downloads/eck/$ECK_VERSION/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/$ECK_VERSION/operator.yaml
+kubectl apply -f $SCRIPT_DIR/logging/config/
+kubectl apply -Rf $SCRIPT_DIR/logging/
+
+# provision application
+kubectl apply -f $SCRIPT_DIR/$ENV/config/
 kubectl apply -Rf $SCRIPT_DIR/$ENV/
